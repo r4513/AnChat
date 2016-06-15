5dcfa58f21adcd53a48a1400a2bc183f4ab8dc7f
= <=>
2016-06-16 00:30:41 +0200

    Initial commit

---
+++ src/AnChat.java
@@ -0,0 +1,15 @@
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class AnChat extends Application{
+
+	public static void main(String[] args){
+		Application.launch(args);
+	}
+	
+	@Override
+	public void start(Stage arg0) throws Exception {
+		
+	}
+
+}
